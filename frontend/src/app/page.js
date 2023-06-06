c167a63e487598a4bcaf52e59291e4d10e1671f3
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-06 08:50:58 -0400

    show locations and times

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -16,24 +16,29 @@ export default function Home() {
   )
 
   return (
-    <div>
-      {dates.map(date => {
-        return <CheckboxComponent
-                  checked={date.isChecked}
-                  key={date.value}
-                  set={setDates}
-                  value={date.value}
-                />
-      })}
-      {dates.map(date => {
-        if (date.checked) {
-          return <DateContainer
-                    date={date.value}
-                    dateData={eventList[date.value]}
-                    key={`${date.value}-container`}
+    <div class="flex-row">
+      <div class="search-params-cont">
+        <h2>Dates</h2>
+        {dates.map(date => {
+          return <CheckboxComponent
+                    checked={date.isChecked}
+                    key={date.value}
+                    set={setDates}
+                    value={date.value}
                   />
-        }
-      })}
+        })}
+      </div>
+      <div>
+        {dates.map(date => {
+          if (date.checked) {
+            return <DateContainer
+                      date={date.value}
+                      dateData={eventList[date.value]}
+                      key={`${date.value}-container`}
+                    />
+          }
+        })}
+      </div>
     </div>
   )
 }
