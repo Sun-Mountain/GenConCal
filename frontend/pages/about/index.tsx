0bb6f4e7ba06a5bd1d910e99d344f750d19725a8
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-12 17:34:19 -0400

    add acknowledgements

--- frontend/pages/about/index.tsx
+++ frontend/pages/about/index.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
 import React, { Component } from "react";
 
 class About extends Component {
   state = {};
   render() {
-    return <main>
-      Haha
-    </main>
+    return (
+      <main>
+        <h3>
+          Acknowledgements
+        </h3>
+        <p>
+          This website was inspired by Jon Schultz's <Link href='https://gencon.eventdb.us/' target='_blank'>GenCon Event Database</Link>.
+        </p>
+      </main>
+    )
   }
 }
 
