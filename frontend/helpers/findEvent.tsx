542193c1769b9c2faf74b26d980fb0b2df4ffec2
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-21 11:35:55 -0400

    init

--- frontend/helpers/findEvent.tsx
+++
@@ -1,5 +0,0 @@
-import { eventData } from "@/pages";
-
-export default function findEvent (eventIndex: number) {
-  return eventData[eventIndex];
-}
--- frontend/interfaces/Data.tsx
+++
@@ -1,7 +0,0 @@
-import { FilterTypes } from "@/interfaces/Filters"
-import { NewEvent } from "@/interfaces/Events"
-
-export interface Data {
-  eventData: Array<NewEvent>,
-  filters: FilterTypes
-}
