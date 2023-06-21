cfec3e5985b1f4ac510e34ed4e7433410921afbe
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-21 13:03:01 -0400

    navigation links

--- frontend/components/layout/Navigation.tsx
+++ frontend/components/layout/Navigation.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '@/assets/images/logo.png';
+
 export default function Navigation () {
   return (
     <nav>
-      navigation
+      <div id="navigation-container">
+        <Image
+          alt='GenCon Calendar Logo'
+          width={60}
+          height={60}
+          src={Logo}
+        />
+        <div id="nav-link-container">
+          <Link href="/">
+            Home
+          </Link>
+          <Link href="/about">
+            About
+          </Link>
+        </div>
+      </div>
     </nav>
   )
 }
