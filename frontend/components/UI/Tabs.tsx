33c488f92ce213ad66ff687bcea29b9a2b39c7ce
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-23 10:06:57 -0400

    update events

--- frontend/components/UI/Tabs.tsx
+++ frontend/components/UI/Tabs.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent, useState } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
@@ -7,15 +7,43 @@ import TabPanel from '@/components/UI/TabPanel';
 import a11yProps from '@/helpers/a11yProps';
 
 import { filterTypes } from '@/pages/_app';
+import { UniqueFilter } from '@/assets/interfaces/Filter';
 
-const days = Object.keys(filterTypes.startDates).sort();
+const eventsListByDay = filterTypes.startDates;
+const eventsListByStartTime = filterTypes.startTimes;
+const dayLabels = Object.keys(eventsListByDay).sort();
+const timeLabels = Object.keys(eventsListByStartTime).sort();
+
+interface CountObj {
+  [index: string]: string
+}
 
 export default function BasicTabs() {
   const [tab, setTab] = useState(0);
+  const [eventCounts, setEventCounts] = useState<CountObj>({})
+
+  const countEvents = (list: UniqueFilter) => {
+    var counts: CountObj = {};
+    dayLabels.map(date => {
+      var num = list[date].length
+      counts[date] = num.toLocaleString("en-US")
+    })
+    setEventCounts(counts)
+  }
+
+  useEffect(() => {
+    countEvents(eventsListByDay)
+  }, [])
 
   const handleChange = (event: SyntheticEvent, newTab: number) => {
     setTab(newTab);
   };
+
+  const getEvents = (day: string) => {
+    const dayEvents = eventsListByDay[day]
+    var eventsForDay = dayEvents;
+    return eventsForDay;
+  }
 
   return (
     <Box id='daily-tabs-container' sx={{ width: '100%' }}>
@@ -27,22 +55,36 @@ export default function BasicTabs() {
           onChange={handleChange}
           aria-label="basic tabs example"
         >
-          {days.map((day, index) => {
+          {dayLabels.map((day, index) => {
+            var eventNum = eventCounts[day],
+                dateLabel = (<div className='tab-label'>
+                  <div className='tab-date'>{day}</div>
+                  <div className='tab-count'>{eventNum} events</div>
+                </div>)
             return (
-              <Tab key={day} label={day} {...a11yProps(index)} />
+              <Tab key={day} label={dateLabel} {...a11yProps(index)} />
             )
           })}
         </Tabs>
       </Box>
-      <TabPanel value={tab} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={tab} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={tab} index={2}>
-        Item Three
-      </TabPanel>
+      {dayLabels.map((day, index) => {
+        var eventsForDay = getEvents(day)
+        return (
+          <TabPanel key={index} value={tab} index={index}>
+            {timeLabels.map(time => {
+              const events = eventsForDay.filter(val => eventsListByStartTime[time].includes(val))
+
+              if (events.length) {
+                return (
+                  <div key={time}>
+                    <h3>{time}</h3>
+                  </div>
+                )
+              }
+            })}
+          </TabPanel>
+        )
+      })}
     </Box>
   );
 }
