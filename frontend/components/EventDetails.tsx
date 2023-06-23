9c933cb816acc92054ce109c50b96953e920db25
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-23 13:20:59 -0400

    set modal

---
+++ frontend/components/EventDetails.tsx
@@ -0,0 +1,9 @@
+import { eventData } from "@/pages/_app";
+
+export default function EventDetails ({ eventIndex }: { eventIndex: number }) {
+  return (
+    <>
+      {eventIndex}
+    </>
+  )
+}
--- frontend/components/UI/TableRow.tsx
+++ frontend/components/UI/TableRow.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+import ModalComponent from '@/components/UI/Modal';
+import EventDetails from '@/components/EventDetails';
 import { eventData } from '@/pages/_app';
+import IconButton from '@mui/material/IconButton'
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 
 export default function TableRow ({ eventIndex }: { eventIndex: number }) {
@@ -9,19 +13,32 @@ export default function TableRow ({ eventIndex }: { eventIndex: number }) {
     playersMax,
     ticketsAvailable
   } = eventData[eventIndex];  
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+
+  const durationPrefix = duration > 1 ? 'hrs' : 'hr';
+
+  const noTickets = ticketsAvailable === 0;
+
   return (
-    <tr key={eventIndex} className={`${ticketsAvailable > 0 ? '' : 'sold-out'}`}>
+    <tr key={eventIndex} className={`${noTickets ? 'sold-out' : ''}`}>
       <td>
         {title}
       </td>
       <td className='center-items'>
-        <ZoomInIcon />
+        <ModalComponent open={open} setOpen={setOpen}>
+          <EventDetails eventIndex={eventIndex} />
+        </ModalComponent>
+        <IconButton className='icon-button' aria-label="zoom in icon" color="secondary" onClick={handleOpen}>
+          <ZoomInIcon />
+        </IconButton>
       </td>
       <td className='center-items'>
         {ticketsAvailable} / {playersMax}
       </td>
       <td className='center-items'>
-        {duration} {duration <= 1 ? 'hr' : 'hrs'}
+        {duration} {durationPrefix}
       </td>
       <td className='center-items'>
         ${cost}
