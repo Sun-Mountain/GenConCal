4a715aeb337c765aefd061acc62131cd8c53273f
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-26 15:01:59 -0400

    filter for xp

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home () {
         </DrawerComponent>
       </div>
       <DailyTabs
-        filterOut={[...filterAgeReq]}
+        filterOut={[...filterAgeReq, ...filterXPReq]}
         hideSoldOut={hideSoldOut}
       />
     </>
