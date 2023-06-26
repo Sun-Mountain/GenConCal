71b87b573b074959ffec25f1dbd9aa7a88d7fb36
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-26 13:31:49 -0400

    reset

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,31 +1,11 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { useState } from 'react';
 import DailyTabs from '@/components/DailyTabs';
 import DrawerComponent from '@/components/UI/Drawer';
 import FilterDrawerContent from '@/components/FilterDrawerContent';
 import ToggleComponent from '@/components/UI/Toggle';
-import { FilterProps } from '@/assets/interfaces';
-
-import { filteredEvents } from './_app';
-
-export const eventFilters = Object.keys(filteredEvents).sort();
-const ageRequirementKeys = Object.keys(filteredEvents.ageRequirement);
-const expRequirementKeys = Object.keys(filteredEvents.experienceType);
 
 export default function Home () {
   const [hideSoldOut, setHideSoldOut] = useState(false);
-  const [ageReqFilter, setAgeReqFilter] = useState<FilterProps>({})
-  const [expReqFilter, setExpReqFilter] = useState<FilterProps>({})
-  
-  useEffect(() => {
-    ageRequirementKeys.map(key => {
-      ageReqFilter[key] = true
-    });
-    setAgeReqFilter(ageReqFilter);
-    expRequirementKeys.map(key => {
-      expReqFilter[key] = true
-    })
-    setExpReqFilter(expReqFilter);
-  }, [ageReqFilter, expReqFilter])
 
   return (
     <>
