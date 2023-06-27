b6f1e6e1f9141a4d93cfb57f9d708bc9aeba5b2e
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-27 15:24:15 -0400

    show event details

--- frontend/components/UI/EventCard.tsx
+++ frontend/components/UI/EventCard.tsx
@@ -1,26 +1,30 @@
 import { NewEvent } from "@/assets/interfaces";
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 
 export default function EventCard({event}: {event: NewEvent}) {
+  const {
+    title,
+    startDate,
+    startTime,
+    endDate,
+    endTime
+  } = event;
+
+  const startDay = (new Date(startDate)).toDateString();
+  const endDay = (new Date(endDate)).toDateString();
+
+  const dateTime = startDate != endDate ? `${startDay},  ${startTime} - ${endDay} ${endTime}` : `${startDay} ${startTime} - ${endTime}`
+
   return (
     <Card className='fave-card' sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+      <CardContent className="fave-content">
+        <div className='fave-title'>{title}</div>
+        <div className='fave-date-container'>
+          {dateTime}
+        </div>
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
