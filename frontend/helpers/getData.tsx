91409f9d625ad328ae7e1179583845746a7214c7
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-26 20:09:13 -0400

    merge with main

--- frontend/helpers/getData.tsx
+++ frontend/helpers/getData.tsx
@@ -1,6 +1,247 @@
+<<<<<<< HEAD
 import Events from '@/assets/events/events.json'
 import cleanData from './cleanData'
 import { RawEvent } from '@/assets/interfaces'
+=======
+import Events from '@/data/events.json';
+
+import { NewEvent, RawEvent } from '@/interfaces/Events';
+import { Data } from '@/interfaces/Data';
+
+const getTime = (time: Date) => {
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  var sHours = hours.toString();
+  var sMinutes = minutes.toString();
+  if(hours<10) sHours = "0" + sHours;
+  if(minutes<10) sMinutes = "0" + sMinutes;
+  return `${sHours}:${sMinutes}`
+}
+
+const isTournament = (eventTournament: string) => {
+  if (eventTournament === "Yes") {
+    return true;
+  }
+
+  return false;
+}
+
+const areMaterialsRequired = (materialsRequired: string) => {
+  if (materialsRequired === "Yes") {
+    return true;
+  }
+
+  return false;
+}
+
+const cleanData = (events: Array<RawEvent>) => {
+  const data: Data = {
+    eventData: [],
+    filters: {
+      ageRequirements: {},
+      costs: {},
+      durationLength: {},
+      endDates: {},
+      endTimes: {},
+      eventTypes: {},
+      experienceRequirements: {},
+      gameSystems: {},
+      groups: {},
+      locations: {},
+      materialsRequired: [],
+      noTickets: [],
+      startDates: {},
+      startTimes: {},
+      tournament: [],
+    }
+  }
+
+  events.map((event, index) => {
+    const newEvent: NewEvent = {
+      id: 0,
+      gameId: '',
+      group: '',
+      title: '',
+      shortDescription: '',
+      longDescription: '',
+      eventType: '',
+      gameSystem: '',
+      ageRequirement: '',
+      experienceRequirement: '',
+      startDate: '',
+      startTime: '',
+      endDate: '',
+      endTime: '',
+      duration: 0,
+      tournament: false,
+      materialsRequired: false,
+      materials: '',
+      cost: 0,
+      location: '',
+      ticketsAvailable: 0,
+      maxTickets: 0
+    };
+
+    const rawStart = new Date(event["Start Date & Time"]);
+    const rawEnd = new Date(event["End Date & Time"]);
+
+    const ageReq = event["Age Required"];
+    const eventCost = Number(event["Cost $"]);
+    const eventDuration = Number(event["Duration"]);
+    const eventEndDate = rawEnd.toLocaleDateString();
+    const eventEndTime = getTime(rawEnd);
+    const eventLocation = event["Location"]?.toUpperCase();
+    const eventStartDate = rawStart.toLocaleDateString();
+    const eventStartTime = getTime(rawStart);
+    const eventTickets = Number(event["Tickets Available"]);
+    const eventType = event["Event Type"];
+    const exp = event["Experience Required"];
+    const gameSystem = event["Game System"];
+    const groupName = event["Group"];
+    const isTourney = isTournament(event["Tournament?"]);
+    const longDescription = event["Long Description"];
+    const materials = event["Materials Required Details"];
+    const materialsRequired = areMaterialsRequired(event["Materials Required"]);
+    const shortDescription = event["Short Description"];
+
+    newEvent.id = index;
+    newEvent.gameId = event["Game ID"];
+    newEvent.maxTickets = Number(event['Maximum Players']);
+
+    if (!data.filters.durationLength[eventDuration]) {
+      data.filters.durationLength[eventDuration] = []
+    }
+    data.filters.durationLength[eventDuration].push(index)
+    newEvent.duration = eventDuration
+
+    // Group Name
+    if (groupName) {
+      if (!data.filters.groups[groupName]) {
+        data.filters.groups[groupName] = []
+      }
+      data.filters.groups[groupName].push(index);
+    }
+    newEvent.group = groupName;
+
+    newEvent.title = event["Title"];
+
+    // Event Name
+    if (eventType) {
+      if (!data.filters.eventTypes[eventType]) {
+        data.filters.eventTypes[eventType] = []
+      }
+      data.filters.eventTypes[eventType].push(index);
+    }
+    newEvent.eventType = eventType;
+
+    // Event Description
+    newEvent.shortDescription = shortDescription;
+    newEvent.longDescription = longDescription;
+
+    // System Name
+    if (gameSystem) {
+      if (!data.filters.gameSystems[gameSystem]) {
+        data.filters.gameSystems[gameSystem] = []
+      }
+      data.filters.gameSystems[gameSystem].push(index);
+    }
+    newEvent.gameSystem = gameSystem;
+
+    // Age Requirement
+    if (ageReq) {
+      if (!data.filters.ageRequirements[ageReq]) {
+        data.filters.ageRequirements[ageReq] = []
+      }
+      data.filters.ageRequirements[ageReq].push(index);
+    }
+    newEvent.ageRequirement = ageReq;
+
+    // Experience Requirement
+    if (exp) {
+      if (!data.filters.experienceRequirements[exp]) {
+        data.filters.experienceRequirements[exp] = []
+      }
+      data.filters.experienceRequirements[exp].push(index);
+    }
+    newEvent.experienceRequirement = exp;
+
+    // Start Date
+    if (eventStartDate) {
+      if (!data.filters.startDates[eventStartDate]) {
+        data.filters.startDates[eventStartDate] = []
+      }
+      data.filters.startDates[eventStartDate].push(index);
+    }
+    newEvent.startDate = eventStartDate;
+
+    // Start Time
+    if (eventStartTime) {
+      if (!data.filters.startTimes[eventStartTime]) {
+        data.filters.startTimes[eventStartTime] = []
+      }
+      data.filters.startTimes[eventStartTime].push(index);
+    }
+    newEvent.startTime = eventStartTime;
+
+    // End Date
+    if (eventEndDate) {
+      if (!data.filters.endDates[eventEndDate]) {
+        data.filters.endDates[eventEndDate] = []
+      }
+      data.filters.endDates[eventEndDate].push(index);
+    }
+    newEvent.endDate = eventEndDate;
+
+    // End Time
+    if (eventEndTime) {
+      if (!data.filters.endTimes[eventEndTime]) {
+        data.filters.endTimes[eventEndTime] = []
+      }
+      data.filters.endTimes[eventEndTime].push(index);
+    }
+    newEvent.endTime = eventEndTime;
+
+    // Tournament
+    if (isTourney) {
+      data.filters.tournament.push(index);
+    };
+    newEvent.tournament = isTourney;
+
+    // Tournament
+    if (materialsRequired) {
+      data.filters.materialsRequired.push(index);
+      newEvent.materials = materials;
+    };
+    newEvent.materialsRequired = materialsRequired;
+
+    // Costs
+    if (!data.filters.costs[eventCost]) {
+      data.filters.costs[eventCost] = []
+    }
+    data.filters.costs[eventCost].push(index);
+    newEvent.cost = eventCost;
+
+    // Locations
+    if (eventLocation) {
+      if (!data.filters.locations[eventLocation]) {
+        data.filters.locations[eventLocation] = []
+      }
+      data.filters.locations[eventLocation].push(index);
+    }
+    newEvent.location = eventLocation;
+
+    // Tickets
+    if (!eventTickets) {
+      data.filters.noTickets.push(index);
+    }
+    newEvent.ticketsAvailable = eventTickets;
+
+    data.eventData.push(newEvent);
+  })
+
+  return data;
+};
+>>>>>>> 1121cd00143d5578b0e87973b6b5698da48b0656
 
 export default function getData() {
   const rawData = Events as RawEvent[]
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,6 +7,7 @@ import DrawerComponent from '@/components/UI/Drawer';
 import EventCategoryFilters from '@/components/EventCategoryFilters';
 import ToggleComponent from '@/components/UI/Toggle';
 
+<<<<<<< HEAD
 import { filteredEvents } from './_app';
 import filterOutHelper from '@/helpers/filterOut';
 import filterForHelper from '@/helpers/filterFor';
@@ -18,9 +19,30 @@ const eventTypeMasterList = filteredEvents.eventTypes;
 const gameSystemMasterList = filteredEvents.gameSystems;
 const groupsMasterList = filteredEvents.groups;
 const locationMasterList = filteredEvents.locations;
+=======
+import DailyTabs from "@/components/DailyTabs";
+import FilterAutoList from "@/components/FilterAutoList";
+import FilterButtons from "@/components/FilterButtons";
+import RadioGroup from "@/components/RadioGroup";
+import SliderComponent from "@/components/SliderComponent";
+import Switch from "@/components/SwitchComponent";
+import TimeRange from "@/components/TimeRange";
+
+export const { eventData, filters } = getData();
+
+export const tournamentFilterOptions = [
+  'Show All Events',
+  'Show Tournament Events Only',
+  'Hide All Tournament Events'
+]
+
+export default function Home() {
+  const filterList = filters;
+>>>>>>> 1121cd00143d5578b0e87973b6b5698da48b0656
 
 export default function Home () {
   // Lists
+<<<<<<< HEAD
   const [ageReqList, setAgeReqList] = useState<string[]>([]);
   const [xpReqList, setXPReqList] = useState<string[]>([]);
   const [eventTypeList, setEventTypeList] = useState<string[]>([]);
@@ -29,6 +51,36 @@ export default function Home () {
   const [locationList, setLocationList] = useState<string[]>([]);
 
   // Filters
+=======
+  const ageRequirements = filterList.ageRequirements;
+  const dateList = filterList.startDates;
+  const durationLength = filterList.durationLength;
+  const eventTypes = filterList.eventTypes;
+  const experienceRequirements = filterList.experienceRequirements;
+  const gameSystems = filterList.gameSystems;
+  const groups = filterList.groups;
+  const locations = filterList.locations;
+  const materialsRequired = filterList.materialsRequired;
+  const soldOutEvents = filterList.noTickets;
+  const startTimes = filterList.startTimes;
+  const tourneyList = filterList.tournament;
+
+  // Filters
+  const [ageFilters, setAgeFilters] = useState<number[]>([]);
+  const [durationFilter, setDurationFilter] = useState<number[]>([0.5, 10]);
+  const [experienceFilters, setExperienceFilters] = useState<number[]>([]);
+  const [eventTypeFilters, setEventTypeFilters] = useState<number[]>([]);
+  const [groupFilter, setGroupFilter] = useState<number[]>([]);
+  const [locationFilter, setLocationFilter] = useState<number[]>([]);
+  const [earlyStartTime, setEarlyStartTime] = useState('00:00');
+  const [lateStartTime, setLateStartTime] = useState('23:45');
+  const [systemFilters, setSystemFilters] = useState<number[]>([]);
+
+  // Radio Group
+  const [tournamentFilter, setTournamentFilter] = useState(tournamentFilterOptions[0]);
+
+  // Switches
+>>>>>>> 1121cd00143d5578b0e87973b6b5698da48b0656
   const [hideSoldOut, setHideSoldOut] = useState(false);
   const [ageReqFilter, setAgeReqFilter] = useState<number[]>([]);
   const [xpFilter, setXPFilter] = useState<number[]>([]);
@@ -117,6 +169,7 @@ export default function Home () {
           hide={hideSoldOut}
           setHide={setHideSoldOut}
         />
+<<<<<<< HEAD
         <div className='drawer-container'>
           <DrawerComponent icon={<FilterAltIcon />} buttonText='Filter By Event Category'>
             <div id='filter-drawer-content-wrapper'>
@@ -158,6 +211,49 @@ export default function Home () {
                     ...groupsFilter,
                     ...locationFilter]}
         filterOut={[...ageReqFilter, ...xpFilter]}
+=======
+        <Switch
+          switchLabel={'Materials Required'}
+          hide={hideMaterialsReq}
+          setHide={setHideMaterialsReq}
+        />
+        <RadioGroup
+          formLabel={'Tournament Filter'}
+          options={tournamentFilterOptions}
+          setValue={setTournamentFilter}
+          value={tournamentFilter}
+        />
+        <TimeRange
+          earlyStartTime={earlyStartTime}
+          lateStartTime={lateStartTime}
+          setEarlyStartTime={setEarlyStartTime}
+          setLateStartTime={setLateStartTime}
+        />
+        <SliderComponent
+          label={'Event Duration (in Hours)'}
+          filterValues={durationFilter}
+          setFilter={setDurationFilter}
+          step={0.5}
+          min={0.5}
+          max={10}
+        />
+      </div>
+      <DailyTabs
+        allBaseFilters={[
+          ...ageFilters,
+          ...experienceFilters
+        ]}
+        showOnly={[
+          eventTypeFilters,
+          systemFilters,
+          groupFilter,
+          locationFilter
+        ]}
+        dateList={dateList}
+        durationFilter={durationFilter}
+        durationLength={durationLength}
+        hideMaterialReq={hideMaterialsReq}
+>>>>>>> 1121cd00143d5578b0e87973b6b5698da48b0656
         hideSoldOut={hideSoldOut}
         tournamentFilter={tournamentFilter}
       />
