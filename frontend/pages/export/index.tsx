3f0023f90e6eb5ffa38e93c60440f2dae23b79fa
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-29 11:54:47 -0400

    filter events

--- frontend/pages/export/index.tsx
+++ frontend/pages/export/index.tsx
@@ -1,9 +1,54 @@
+import { useEffect } from 'react';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import { eventData, filteredEvents } from "@/pages/_app";
 import getQuarterHours from "@/helpers/getQuarterHours";
+import { UniqueFilter } from '@/assets/interfaces';
+
+const timeLabels = getQuarterHours();
+
+const dayLabels = Object.keys(filteredEvents.startDates).sort();
+const times = Object.keys(filteredEvents.startTimes).sort();
+
+interface TimeFilterProps {
+  [index: string]: number[];
+}
+
+interface FilteredFavesProps {
+  [index: string]: TimeFilterProps;
+}
+
+
+const organizeFilters = (
+  faves: number[],
+  timeFilter: UniqueFilter,
+  dayFilter: UniqueFilter
+) => {
+  var filteredFaves: FilteredFavesProps = {}
+  dayLabels.map(dayLabel => {
+    filteredFaves[dayLabel] = {}
+    timeLabels.map(timeLabel => {
+      filteredFaves[dayLabel][timeLabel] = [];
+    })
+  })
+
+  faves.map(fave => {
+    var { startDate, startTime } = eventData[fave];
+    filteredFaves[startDate][startTime].push(fave)
+  })
+  console.log(filteredFaves)
+}
 
 export default function ExportPage () {
   const faves = JSON.parse(localStorage.getItem('faves') || '[]');
+
+  useEffect(() => {
+    if (faves.length) {
+      organizeFilters(faves,
+                      filteredEvents.startTimes,
+                      filteredEvents.startDates,
+                      )
+    }
+  }, [faves])
 
   if (!faves || !faves.length) {
     return (
@@ -12,12 +57,6 @@ export default function ExportPage () {
       </>
     )
   }
-
-  const dayLabels = Object.keys(filteredEvents.startDates).sort();
-  const timeLabels = Object.keys(filteredEvents.startTimes).sort();
-  const times = getQuarterHours;
-
-
 
   return (
     <>
@@ -39,6 +78,7 @@ export default function ExportPage () {
           </thead>
           <tbody>
             {timeLabels.map(timeLabel => {
+              // var favesFilteredByTime = favesByTime(timeLabel)
               return (
                 <tr key={`${timeLabel}-row`}>
                   <td>
