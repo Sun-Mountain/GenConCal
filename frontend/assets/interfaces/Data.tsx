248f1c7e35cdc5c0e1f8bfd116a317ca589cb11b
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-07-11 14:49:32 -0400

    identify keys

--- frontend/assets/interfaces/Data.tsx
+++ frontend/assets/interfaces/Data.tsx
@@ -9,3 +9,73 @@ export interface DataProps {
 export interface CountObj {
   [index: string]: number
 }
+
+export interface KeyProps {
+  A: 'Game ID',
+  B: 'Group',
+  C: 'Title',
+  D: 'Short Description',
+  E: 'Long Description',
+  F: 'Event Type',
+  G: 'Game System',
+  H: 'Rules Edition',
+  I: 'Minimum Players',
+  J: 'Maximum Players',
+  K: 'Age Required',
+  L: 'Experience Required',
+  M: 'Materials Required',
+  N: 'Materials Required Details',
+  O: 'Start Date & Time',
+  P: 'Duration',
+  Q: 'End Date & Time',
+  R: 'GM Names',
+  S: 'Website',
+  T: 'Email',
+  U: 'Tournament?',
+  V: 'Round Number',
+  W: 'Total Rounds',
+  X: 'Minimum Play Time',
+  Y: 'Attendee Registration?',
+  Z: 'Cost $',
+  AA: 'Location',
+  AB: 'Room Name',
+  AC: 'Table Number',
+  AD: 'Special Category',
+  AE: 'Tickets Available',
+  AF: 'Last Modified'
+}
+
+export interface EventListingProps {
+  A: string;
+  B: string;
+  C: string;
+  D: string;
+  E: string;
+  F: string;
+  G: string;
+  H: string;
+  I: number;
+  J: number;
+  K: string;
+  L: string;
+  M: string;
+  N: string;
+  O: string;
+  P: number;
+  Q: string;
+  R: string;
+  S: string;
+  T: string;
+  U: string;
+  V: number;
+  W: number;
+  X: string;
+  Y: string;
+  Z: number;
+  AA: string;
+  AB: number;
+  AC: string;
+  AD: string;
+  AE: number;
+  AF: string;
+}
--- frontend/helpers/parseData.tsx
+++ frontend/helpers/parseData.tsx
@@ -1,9 +1,76 @@
 import EventData from '@/assets/events/rawEvents.json';
+import { KeyProps, EventListingProps, DataProps } from '@/assets/interfaces';
 
 const getFirstKey = (object: any) => {
   const keyList = Object.keys(object)
 
   return keyList[0]
+}
+
+const getKeyByValue = (object: any, value: string) => {
+  const foundKey = Object.keys(object).find((key) => object[key] === value);
+  return foundKey;
+}
+
+const cleanData = ({ keyList, eventList }: {
+  keyList: KeyProps;
+  eventList: EventListingProps[];
+}) => {
+  var data: DataProps = {
+    eventData: [],
+    filteredEvents: {
+      ageRequirement: {},
+      cost: {},
+      duration: {},
+      endDates: {},
+      endTimes: {},
+      eventTypes: {},
+      experienceType: {},
+      gameSystems: {},
+      groups: {},
+      locations: {},
+      materialsRequired: [],
+      noTickets: [],
+      startDates: {},
+      startTimes: {},
+      tournaments: []
+    }
+  }
+
+  // Event Times
+  const startDateTimeKey = getKeyByValue(keyList, "Start Date & Time");
+  const endDateTimeKey = getKeyByValue(keyList, "End Date & Time");
+
+  // Event Info
+  const ageRequirementKey = getKeyByValue(keyList, "Age Required");
+  const contactKey = getKeyByValue(keyList, "Email");
+  const costKey = getKeyByValue(keyList, "Cost $");
+  const durationKey = getKeyByValue(keyList, "Duration");
+  const descriptionShortKey = getKeyByValue(keyList, "Short Description");
+  const descriptionLongKey = getKeyByValue(keyList, "Long Description");
+  const eventTypeKey = getKeyByValue(keyList, "Event Type");
+  const expKey = getKeyByValue(keyList, "Experience Required");
+  const gameIdKey = getKeyByValue(keyList, "Game ID");
+  const gameSystemKey = getKeyByValue(keyList, "Game System");
+  const gmNamesKey = getKeyByValue(keyList, "GM Names");
+  const groupKey = getKeyByValue(keyList, "Group");
+  const locationKey = getKeyByValue(keyList, "Location");
+  const materialsKey = getKeyByValue(keyList, "Materials Required Details");
+  const playersMinKey = getKeyByValue(keyList, "Minimum Players");
+  const playersMaxKey = getKeyByValue(keyList, "Maximum Players");
+  const tableNumKey = getKeyByValue(keyList, "Table Number");
+  const ticketCountKey = getKeyByValue(keyList, "Tickets Available");
+  const titleKey = getKeyByValue(keyList, "Title");
+  const tournamentKey = getKeyByValue(keyList, "Tournament?");
+  const roomKey = getKeyByValue(keyList, "Room Name");
+  const roundKey = getKeyByValue(keyList, "Round Number");
+  const roundTotalKey = getKeyByValue(keyList, "Total Rounds");
+  const websiteKey = getKeyByValue(keyList, "Website");
+
+  eventList.map((event, index) => {
+  })
+
+  return data;
 }
 
 export default function parseData () {
@@ -24,4 +91,6 @@ export default function parseData () {
 
   // Events list
   const rawEventsList = rawJsonValues;
+
+  cleanData({ keyList: labelKey, eventList: rawEventsList });
 };
