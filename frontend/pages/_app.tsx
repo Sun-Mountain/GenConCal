16379a027445d99b9e4aed2a0be7b070b13e5c1a
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-07-12 10:33:44 -0400

    organize helper files

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,7 +6,6 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react'
 
-import getData from "@/helpers/getData"
 import parseData from "@/helpers/parseData"
 
 export const { eventData, filteredEvents } = parseData()
