0782bec51c9a1d97a2051ff43d3d066d28448f1d
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-07-12 10:47:56 -0400

    update date

--- frontend/components/UI/Navigation.tsx
+++ frontend/components/UI/Navigation.tsx
@@ -1,31 +1,39 @@
 import Link from "next/link";
-import EventIcon from '@mui/icons-material/Event';
 import CottageIcon from '@mui/icons-material/Cottage';
 import InfoIcon from '@mui/icons-material/Info';
+import lastUpdated from '@/assets/events/lastUpdate.json';
+
+console.log(lastUpdated)
 
 export default function Navigation () {
+
+  const dateValue = JSON.parse(lastUpdated.date);
+  // const newDate = new Date(dateValue);
+
+  console.log(dateValue)
+
   return (
     <nav>
-      <div id='navigation-container'>
-        <div id='nav-title'>
-          <Link className='nav-link' href='/'>
-            <EventIcon fontSize='large' />
-          </Link>
+      <div id='nav-wrapper'>
+        <div id='navigation-links-container'>
+          <ul id='nav-link-container'>
+            <li className='link-container'>
+              <Link className='nav-link' href='/'>
+                <CottageIcon />&nbsp;
+                Home
+              </Link>
+            </li>
+            <li className='link-container'>
+              <Link className='nav-link' href='/about'>
+                <InfoIcon />&nbsp;
+                About
+              </Link>
+            </li>
+          </ul>
         </div>
-        <ul id='nav-link-container'>
-          <li className='link-container'>
-            <Link className='nav-link' href='/'>
-              <CottageIcon />&nbsp;
-              Home
-            </Link>
-          </li>
-          <li className='link-container'>
-            <Link className='nav-link' href='/about'>
-              <InfoIcon />&nbsp;
-              About
-            </Link>
-          </li>
-        </ul>
+        <div>
+          {/* {dateValue} */}
+        </div>
       </div>
     </nav>
   )
