40bfc2a7be07be94da2568e02134166e8d49d818
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-07-25 11:37:32 -0400

    fix long description

--- frontend/helpers/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from '@/helpers/a11yHelpers';
-export * from '@/helpers/cleanData';
-export * from '@/helpers/filterHelpers';
-export * from '@/helpers/finderHelpers';
-export * from '@/helpers/getHelpers';
-export * from '@/helpers/parsingHelpers';
