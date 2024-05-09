3866bc862054a266a9c79bf115f436c09978b163
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-05-09 16:44:14 -0400

    sold out button

--- frontend/src/components/HideShowButton/index.tsx
+++ frontend/src/components/HideShowButton/index.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+
 import Button from '@components/UI/Button';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
@@ -13,11 +17,19 @@ const index = ({
   onClick,
   buttonTitle
 }: Props) => {
-  const icon = show ? <VisibilityIcon /> : <VisibilityOffIcon />;
+  const [visible, setVisible] = useState(show);
+  const icon = visible ? <VisibilityIcon /> : <VisibilityOffIcon />;
+
+  const handleClick = () => {
+    if (onClick) onClick();
+    setVisible(!visible);
+  }
 
   return (
-    <Button>
-      <div className='icon-container'>
+    <Button
+      onClick={handleClick}
+    >
+      <div className='hide-show-icon'>
         {icon}
       </div>
       {buttonTitle}
