0a86cbd31381af8e24484c0562b86b9a250e9e14
Evan Rittenhouse <emanguy1@gmail.com>
2025-04-25 21:16:18 -0400

    WIP filling out adapters for events

--- backend/src/persistence/event.rs
+++ backend/src/persistence/event.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use anyhow::{Context, Error};
 use chrono::Datelike;
 use crate::domain;
-use crate::domain::event::{CreateParams, UpdateParams};
+use crate::domain::event::{AgeRequirement, CreateParams, ExperienceLevel, UpdateParams};
 use crate::external_connections::{ConnectionHandle, ExternalConnectivity};
 
 struct GameIdAndId {
@@ -37,11 +37,89 @@ impl domain::event::driven_ports::EventDetector for DbEventDetector {
     }
 }
 
+#[derive(sqlx::Type)]
+#[sqlx(type_name = "agerequirement")]
+enum AgeRequirementDTO {
+    Everyone,
+    KidsOnly,
+    Teen,
+    Mature,
+    Adult,
+}
+
+impl From<AgeRequirement> for AgeRequirementDTO {
+    fn from(age_req: AgeRequirement) -> Self {
+        match age_req {
+            AgeRequirement::Everyone => AgeRequirementDTO::Everyone,
+            AgeRequirement::KidsOnly => AgeRequirementDTO::KidsOnly,
+            AgeRequirement::Teen => AgeRequirementDTO::Teen,
+            AgeRequirement::Mature => AgeRequirementDTO::Mature,
+            AgeRequirement::Adult => AgeRequirementDTO::Adult,
+        }
+    }
+}
+
+#[derive(sqlx::Type)]
+#[sqlx(type_name = "experiencerequirement")]
+enum ExperienceLevelDTO {
+    Some,
+    None,
+    Expert,
+}
+
+impl From<ExperienceLevel> for ExperienceLevelDTO {
+    fn from(exp_req: ExperienceLevel) -> Self {
+        match exp_req {
+            ExperienceLevel::None => ExperienceLevelDTO::None,
+            ExperienceLevel::Some => ExperienceLevelDTO::Some,
+            ExperienceLevel::Expert => ExperienceLevelDTO::Expert,
+        }
+    }
+}
+
 pub struct DbEventWriter;
 
 impl domain::event::driven_ports::EventWriter for DbEventWriter {
     async fn bulk_save_events(&self, create_params: &[CreateParams<'_>], ext_cxn: &mut impl ExternalConnectivity) -> Result<Vec<i64>, Error> {
-        todo!()
+        let mut cxn = ext_cxn.database_cxn().await.context("Retrieving database connection to save events")?;
+        
+        let mut saved_ids: Vec<i64> = Vec::with_capacity(create_params.len());
+        
+        for create_param in create_params.iter() {
+            let age_req = AgeRequirementDTO::from(create_param.age_requirement);
+            let exp_req = ExperienceLevelDTO::from(create_param.experience_level);
+            
+            let inserted_id = sqlx::query_as!(
+                i64, 
+                r#"
+                INSERT INTO events(
+                    game_id, game_system_id, event_type_id, 
+                    age_requirement, required_experience, table_number, 
+                    materials_id, contact_id, website_id, group_id, 
+                    title, description, start_dt, end_dt, 
+                    year, cost, tickets_available, min_players, max_players
+                ) 
+                VALUES (
+                    $1, $2, $3, 
+                    $4, $5, $6, 
+                    $7, $8, $9, $10, 
+                    $11, $12, $13, $14, 
+                    date_part('year', CURRENT_DATE), $15, $16, $17, $18
+                ) 
+                RETURNING id
+                "#,
+                create_param.game_id, create_param.game_system_id, create_param.event_type_id,
+                age_req, exp_req, create_param.table_number,
+                create_param.materials_id, create_param.contact_id, create_param.website_id, create_param.group_id,
+                create_param.title, create_param.description, create_param.start_dt, create_param.end_dt,
+                create_param.cost, create_param.tickets_available, create_param.min_players, create_param.max_players
+            ).fetch_one()
+                .context("Inserting a new event into the database")?;
+            
+            // TODO save the ID and cantinue from here
+        }
+        
+        Ok(saved_ids)
     }
 
     async fn bulk_update_events(&self, update_params: &[(i64, UpdateParams<'_>)], ext_cxn: &mut impl ExternalConnectivity) -> Result<(), Error> {
