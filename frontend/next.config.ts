f877c144dd6d0dfd2fb9e5f58fa10bcfa1a82b9d
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2025-07-16 13:57:24 -0400

    start styling

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+const path = require('path')
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
 export default nextConfig;
---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,7 @@
+export function Navigation () {
+    return (
+        <nav>
+            Navigation
+        </nav>
+    )
+}
